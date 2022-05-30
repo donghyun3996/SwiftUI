0e63264a8642809598de6740d64267d02f575991
이동현 <81354005+donghyun3996@users.noreply.github.com>
2022-05-30 23:32:31 +0900

    Add files via upload

---
+++ SwiftUI_Stack_tutorial/SwiftUI_Stack_tutorial/MyNavigationView.swift
@@ -0,0 +1,22 @@
+//
+//  MyNavigationView.swift
+//  SwiftUI_Stack_tutorial
+//
+//  Created by 동현 on 2022/05/30.
+//
+
+import SwiftUI
+
+struct MyNavigarionView: View{
+    var body: some View{
+        Text("MyNavigationView")
+          
+    }
+}
+
+
+struct MyNavigarionView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyNavigarionView ()
+    }
+}
