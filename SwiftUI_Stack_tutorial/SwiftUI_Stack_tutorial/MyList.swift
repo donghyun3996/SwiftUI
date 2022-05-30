73cb2e0717338cf5fe8eb124d0808e549d05f4ac
이동현 <81354005+donghyun3996@users.noreply.github.com>
2022-05-30 23:06:32 +0900

    Add files via upload

---
+++ SwiftUI_Stack_tutorial/SwiftUI_Stack_tutorial/MyList.swift
@@ -0,0 +1,52 @@
+//
+//  MyList.swift
+//  SwiftUI_Stack_tutorial
+//
+//  Created by 동현 on 2022/05/30.
+//
+
+import SwiftUI
+
+struct MyList: View{
+    
+    var body: some View{
+//        List{
+//            Text("마이리스트")
+//            Text("마이리스트")
+//            Text("마이리스트")
+//            Text("마이리스트")
+//            Text("마이리스트")
+//            Text("마이리스트")
+//
+//        }
+        List{
+            
+            Section(header: Text("오늘 할 일")
+                .font(.headline)
+                .foregroundColor(Color.black)
+                , footer: Text("footer")){
+             
+            ForEach(1...3, id: \.self){ itemIndex in
+                MyCard(icon: "book.fill", title: "책읽기\(itemIndex)", start: "1 PM", end: "2 PM", bgColor: Color.green)
+            }
+        }
+        .listRowInsets(EdgeInsets.init(top: 10, leading: 10, bottom: 10, trailing: 10 ))
+            Section(header: Text("내일 할 일").font(.headline)
+                .foregroundColor(Color.black)){
+                ForEach(1...3, id: \.self){ itemIndex in
+                    MyCard(icon: "book.fill", title: "책읽기\(itemIndex)", start: "1 PM", end: "2 PM", bgColor: Color.blue)
+                }
+            }
+            .listRowInsets(EdgeInsets.init(top: 10, leading: 10, bottom: 10, trailing: 10 ))
+            .listRowBackground(Color.yellow)
+        }.listStyle(GroupedListStyle())
+
+    }
+   
+}
+    struct MyList_Previews: PreviewProvider {
+        static var previews: some View {
+            MyList()
+        }
+    }
+
