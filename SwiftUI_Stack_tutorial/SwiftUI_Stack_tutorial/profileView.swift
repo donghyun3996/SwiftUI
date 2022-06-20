23e7a9eccfa02a53410ea4c67e800b174829c671
이동현 <81354005+donghyun3996@users.noreply.github.com>
2022-06-20 23:34:58 +0900

    Add files via upload

---
+++ SwiftUI_Stack_tutorial/SwiftUI_Stack_tutorial/profileView.swift
@@ -0,0 +1,30 @@
+//
+//  profileView.swift
+//  SwiftUI_Stack_tutorial
+//
+//  Created by 동현 on 2022/06/20.
+//
+
+import SwiftUI
+
+struct profileView: View{
+    var body: some View{
+        NavigationView{
+            navigationTitle("내 프로필")
+            navigationBarItems( trailing:
+                                    Button(
+                                        action:{ print("호호")
+                                        }){
+                                            Image(systemName: "gear")
+                                        }
+            )
+        }
+    }
+}
+
+
+struct profileView_Previews: PreviewProvider {
+    static var previews: some View {
+        profileView()
+    }
+}
