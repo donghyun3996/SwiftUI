036b3c521a5719d13f77ea4d5de16e2e7656deed
이동현 <81354005+donghyun3996@users.noreply.github.com>
2022-06-22 23:09:36 +0900

    Add files via upload

---
+++ SwiftUI_Stack_tutorial/SwiftUI_Stack_tutorial/MyGeometryReader.swift
@@ -0,0 +1,23 @@
+//
+//  MyGeometryReader.swift
+//  SwiftUI_Stack_tutorial
+//
+//  Created by 동현 on 2022/06/22.
+//
+
+import SwiftUI
+
+struct MyGeometryReader: View{
+    var body: some View{
+        Text("MyGeometryReader")
+    }
+}
+
+
+struct MyGeometryReader_Previews: PreviewProvider {
+    static var previews: some View {
+        MyGeometryReader()
+        
+    }
+    
+}
