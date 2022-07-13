6c8fde94c85171065946f8d59853c97fde2d80ed
이동현 <81354005+donghyun3996@users.noreply.github.com>
2022-07-13 10:47:02 +0900

    Add files via upload

--- SwiftUI_Stack_tutorial/SwiftUI_Stack_tutorial/MyCustomTabView.swift
+++ SwiftUI_Stack_tutorial/SwiftUI_Stack_tutorial/MyCustomTabView.swift
@@ -15,6 +15,8 @@ struct MyCustomtabView: View{
     
     @State var tabIndex : TabIndex
     
+    @State var largerScale : CGFloat = 1.5
+    
     func changeMyView (tabIndex: TabIndex) -> MyView{
         switch tabIndex {
         case .home:
@@ -29,37 +31,96 @@ struct MyCustomtabView: View{
         }
     }
     
+    func changeColor (tabIndex: TabIndex) -> Color{
+        switch tabIndex {
+        case .home:
+            return Color.green
+        case .cart:
+            return Color.purple
+        case .profile:
+            return Color.blue
+        default:
+            return Color.green
+ 
+        }
+    }
+    
+    func calcCircleBgPosition(tabIndex: TabIndex, G: GeometryProxy) -> CGFloat {
+        switch tabIndex {
+        case .home:
+            return -(G.size.width / 3)
+        case .cart:
+            return 0
+        case .profile:
+            return (G.size.width / 3)
+        default:
+            return -(G.size.width / 2)
+ 
+        }
+    }
+    
     var body: some View
     {
         GeometryReader{ G in
             ZStack(alignment: .bottom){
             self.changeMyView(tabIndex: self.tabIndex)
+                
+                Circle()
+                    .frame(width: 90, height: 90)
+                    .offset(x: self.calcCircleBgPosition(tabIndex: self.tabIndex, G: G), y: 20)
+                    .foregroundColor(Color.white)
+                
                 HStack(spacing: 0){
                     
                     Button(action:{
                         print("홈 클릭")
+                        
+                        withAnimation{
+                            self.tabIndex = .home
+                        }
+                        
+                        
                     }){
                         Image(systemName: "house.fill")
                             .font(.system(size: 25))
                             .frame(width: G.size.width / 3, height: 40)
-                    }.background(Color.orange)
+                            .scaleEffect(self.tabIndex == .home ?  self.largerScale : 1.0)
+                            .foregroundColor(self.tabIndex == .home ? self.changeColor(tabIndex: self.tabIndex) : Color.gray)
+                            .offset(y: self.tabIndex == .home ? -10 : 0)
+                    }.background(Color.white)
                     
                     
                     Button(action:{
                         print("장바구니 클릭")
+                        
+                        withAnimation{
+                            self.tabIndex = .cart
+
+                        }
                     }){
                         Image(systemName: "cart.fill")
                             .font(.system(size: 25))
                             .frame(width: G.size.width / 3, height: 40)
-                    }.background(Color.yellow)
+                            .scaleEffect(self.tabIndex == .cart ?  self.largerScale : 1.0)
+                            .foregroundColor(self.tabIndex == .cart ? self.changeColor(tabIndex: self.tabIndex) : Color.gray)
+                            .offset(y: self.tabIndex == .cart ? -10 : 0)
+                    }.background(Color.white)
                     
                     
                     Button(action:{
                         print("프로필 클릭")
+                        
+                        withAnimation{
+                            self.tabIndex = .profile
+
+                        }
                     }){
                         Image(systemName: "person.circle.fill")
                             .font(.system(size: 25))
                             .frame(width: G.size.width / 3, height: 40)
+                            .scaleEffect(self.tabIndex == .profile ?  self.largerScale : 1.0)
+                            .foregroundColor(self.tabIndex == .profile ? self.changeColor(tabIndex: self.tabIndex) : Color.gray)
+                            .offset(y: self.tabIndex == .profile ? -10 : 0)
                     }.background(Color.white)
                 }
             }
